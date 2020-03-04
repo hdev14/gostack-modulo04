3ca0eb34abbdfdc6ca1d54c0e36678dddc8d9a58
HermersonDev <ne.hermerson@gmail.com>
2020-03-03 21:32:22 -0300

    :construction: creates handler to remove tech

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -10,13 +10,19 @@ class TechList extends React.Component {
     ]
   }
 
-  handlerInputChange = e => {
+  handleInputChange = e => {
     this.setState({
       newTech: e.target.value
     });
   }
 
-  handlerOnSubmit = e => {
+  handleDelete(tech) {
+    this.setState({
+      techs: this.state.techs.filter(t => t !== tech)
+    });
+  }
+
+  handleSubmit = e => {
     e.preventDefault();
     this.setState({
       newTech: '',
@@ -31,17 +37,24 @@ class TechList extends React.Component {
     const { techs } = this.state;
 
     return (
-      <form onSubmit={this.handlerOnSubmit}>
+
+      <form onSubmit={this.handleSubmit}>
         <h1>{this.state.newTech}</h1>
         <ul>
-          {techs.map(tech => <li key={tech}>{tech}</li>)}
+          {techs.map(tech => (
+            <li key={tech}>
+              {tech}
+              <button type='button' onClick={() => this.handleDelete(tech)}> Remover </button>
+            </li>
+          ))}
         </ul>
         <input 
           type="text" 
-          onChange={this.handlerInputChange} 
+          onChange={this.handleInputChange} 
           value={this.state.newTech} />
         <button type="submit">Enviar</button>
       </form>
+
     );
   }
 }
